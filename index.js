4e4b4fd6e09c0f733d1982481af9f41a58e45e3d
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-03-25 03:08:08 +0500

    adding cors to the server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import cors from "cors";
 
 import usersRoutes from "./routes/users.js";
 
@@ -11,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/users', usersRoutes);
 
